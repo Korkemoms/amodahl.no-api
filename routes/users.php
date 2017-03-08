5acea614269165f6258a1047fc8ad8c7a139dd73
amo102 <a_modahl@hotmail.com>
2017-03-09 00:27:45 +0100

    Removed unused routes, added update index

--- routes/users.php
+++ routes/users.php
@@ -20,70 +20,22 @@ $app->get("/users", function ($request, $response, $arguments) {
         throw new ForbiddenException("Token not allowed to list users.", 403);
     }
 
-    /* Use ETag and date from user with most recent update. */
-    $first = $this->spot->mapper("App\user")
-        ->all()
-        ->order(["updated_at" => "DESC"])
-        ->first();
+    if ($request->hasHeader('update-index')) {
+      $updateIndex = $request->getHeader('update-index')[0];
 
-    /* Add Last-Modified and ETag headers to response when atleast one user exists. */
-    if ($first) {
-        $response = $this->cache->withEtag($response, $first->etag());
-        $response = $this->cache->withLastModified($response, $first->timestamp());
-    }
-
-    /* If-Modified-Since and If-None-Match request header handling. */
-    /* Heads up! Apache removes previously set Last-Modified header */
-    /* from 304 Not Modified responses. */
-    if ($this->cache->isNotModified($request, $response)) {
-        return $response->withStatus(304);
-    }
-
-    $users = $this->spot->mapper("App\user")
-        ->all()
-        ->order(["updated_at" => "DESC"]);
-
-
-    /* Serialize the response data. */
-    $fractal = new Manager();
-    $fractal->setSerializer(new DataArraySerializer);
-    $resource = new Collection($users, new userTransformer);
-    $data = $fractal->createData($resource)->toArray();
-
-    return $response->withStatus(200)
-        ->withHeader("Content-Type", "application/json")
-        ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-});
-
-$app->get("/users/{uid}", function ($request, $response, $arguments) {
-
-    /* Check if token has needed scope. */
-    if (false === $this->token->hasScope(["user.all", "user.read"])) {
-        throw new ForbiddenException("Token not allowed to list users.", 403);
-    }
-
-    /* Load existing user using provided uid */
-    if (false === $user = $this->spot->mapper("App\user")->first([
-        "uid" => $arguments["uid"]
-    ])) {
-        throw new NotFoundException("user not found.", 404);
-    };
-
-    /* Add Last-Modified and ETag headers to response. */
-    $response = $this->cache->withEtag($response, $user->etag());
-    $response = $this->cache->withLastModified($response, $user->timestamp());
-
-    /* If-Modified-Since and If-None-Match request header handling. */
-    /* Heads up! Apache removes previously set Last-Modified header */
-    /* from 304 Not Modified responses. */
-    if ($this->cache->isNotModified($request, $response)) {
-        return $response->withStatus(304);
+      $users = $this->spot->mapper("App\user")
+          ->where(['update_index >' => $updateIndex])
+          ->order(["updated_at" => "DESC"]);
+    }else{
+      $users = $this->spot->mapper("App\user")
+          ->all()
+          ->order(["updated_at" => "DESC"]);
     }
 
     /* Serialize the response data. */
     $fractal = new Manager();
     $fractal->setSerializer(new DataArraySerializer);
-    $resource = new Item($user, new userTransformer);
+    $resource = new Collection($users, new UserTransformer);
     $data = $fractal->createData($resource)->toArray();
 
     return $response->withStatus(200)
