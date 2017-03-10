fb14102c65d7ebf79d9009e34cb2c85d953dce5c
amo102 <a_modahl@hotmail.com>
2017-03-10 22:24:05 +0100

    Added moves to update route

--- routes/updates.php
+++ routes/updates.php
@@ -6,6 +6,9 @@ use App\UserTransformer;
 use App\ChessGame;
 use App\ChessGameTransformer;
 
+use App\ChessMove;
+use App\ChessMoveTransformer;
+
 use Exception\NotFoundException;
 use Exception\ForbiddenException;
 use Exception\PreconditionFailedException;
@@ -16,25 +19,30 @@ use League\Fractal\Resource\Item;
 use League\Fractal\Resource\Collection;
 use League\Fractal\Serializer\DataArraySerializer;
 
+/* Clients can requeste this periodically to stay updated. */
+// TODO update_index should be indexed in the database to improve speed
 $app->get("/updates", function ($request, $response, $arguments) {
 
     $email = $this["token"]->decoded->email;
-    
+
     /* Check if token has needed scope. */
     if (false === $this->token->hasScope(["update.all", "update.list"])) {
         throw new ForbiddenException("Token not allowed to list updates.", 403);
     }
 
+    $updateIndex = $request->hasHeader('update-index')
+      ? $request->getHeader('update-index')[0] : -1;
+
+
     // get updated players
-    if ($request->hasHeader('update-index')) {
-      $updateIndex = $request->getHeader('update-index')[0];
+    if ($updateIndex !== -1) {
       $users = $this->spot->mapper("App\user")
           ->where(['update_index >' => $updateIndex])
-          ->order(["updated_at" => "DESC"]);
+          ->order(["update_index" => "DESC"]);
     }else{
       $users = $this->spot->mapper("App\user")
           ->all()
-          ->order(["updated_at" => "DESC"]);
+          ->order(["update_index" => "DESC"]);
     }
     $fractal = new Manager();
     $fractal->setSerializer(new DataArraySerializer);
@@ -44,23 +52,40 @@ $app->get("/updates", function ($request, $response, $arguments) {
 
     // get updated chess games
     if ($request->hasHeader('update-index')) {
-      $updateIndex = $request->getHeader('update-index')[0];
       $chessGames = $this->spot->mapper("App\ChessGame")
           ->where(['update_index >' => $updateIndex])
-          ->order(["updated_at" => "DESC"]);
+          ->order(["update_index" => "DESC"]);
     }else{
       $chessGames = $this->spot->mapper("App\ChessGame")
           ->all()
-          ->order(["updated_at" => "DESC"]);
+          ->order(["update_index" => "DESC"]);
     }
     $fractal = new Manager();
     $fractal->setSerializer(new DataArraySerializer);
     $resource = new Collection($chessGames, new ChessGameTransformer);
     $chessGameData = $fractal->createData($resource)->toArray();
 
+
+    // get updated chess moves
+    if ($request->hasHeader('update-index')) {
+      $chessMoves = $this->spot->mapper("App\ChessMove")
+          ->where(['update_index >' => $updateIndex])
+          ->order(["update_index" => "DESC"]);
+    }else{
+      $chessMoves = $this->spot->mapper("App\ChessMove")
+          ->all()
+          ->order(["update_index" => "DESC"]);
+    }
+    $fractal = new Manager();
+    $fractal->setSerializer(new DataArraySerializer);
+    $resource = new Collection($chessMoves, new ChessMoveTransformer);
+    $chessMoveData = $fractal->createData($resource)->toArray();
+
+
     $data = [
       "players" => $playerData,
-      "chessGames" => $chessGameData
+      "chessGames" => $chessGameData,
+      "chessMoves" => $chessMoveData
     ];
 
     return $response->withStatus(200)
